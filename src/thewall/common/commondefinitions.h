1d1e8255f85f702bcffa61b8eb39b1e52c757ab3
Sungwon Nam <schiktol@gmail.com>
2012-02-27 13:19:45 -0600

    Definitions are moved to here

--- src/thewall/common/commondefinitions.h
+++ src/thewall/common/commondefinitions.h
@@ -27,10 +27,46 @@
 //
 #define INTERACTIVE_ITEM 10
 
+
+#define EXTUI_MSG_SIZE 1280
+#define EXTUI_SMALL_MSG_SIZE 128
+
+
 namespace SAGENext {
 
 enum MEDIA_TYPE { MEDIA_TYPE_UNKNOWN = 100, MEDIA_TYPE_IMAGE, MEDIA_TYPE_VIDEO, MEDIA_TYPE_LOCAL_VIDEO, MEDIA_TYPE_AUDIO, MEDIA_TYPE_PLUGIN , MEDIA_TYPE_VNC, MEDIA_TYPE_WEBURL, MEDIA_TYPE_PDF , MEDIA_TYPE_SAGE_STREAM};
 
+
+/* transfer file / stream pixel / stream file */
+enum EXTUI_TRANSFER_MODE { FILE_TRANSFER, FILE_STREAM, PIXEL_STREAM };
+
+
+/* types of messages between external UI and the wall */
+enum EXTUI_MSG_TYPE { MSG_NULL
+	                  , REG_FROM_UI
+	                  , ACK_FROM_WALL
+	                  , DISCONNECT_FROM_WALL
+	                  , WALL_IS_CLOSING
+	                  , TOGGLE_APP_LAYOUT, RESPOND_APP_LAYOUT /* not used */
+
+	                  , VNC_SHARING
+
+	                  , POINTER_PRESS, POINTER_RIGHTPRESS
+	                  , POINTER_RELEASE, POINTER_RIGHTRELEASE
+	                  , POINTER_CLICK, POINTER_RIGHTCLICK, POINTER_DOUBLECLICK
+	                  , POINTER_DRAGGING, POINTER_RIGHTDRAGGING, POINTER_MOVING
+	                  , POINTER_SHARE, POINTER_WHEEL, POINTER_UNSHARE
+
+	                  , RESPOND_STRING
+
+	                  /*
+	                  Below is to interact with widget directly from a uiclient
+	                  */
+	                  , WIDGET_Z
+	                  , WIDGET_REMOVE
+	                  , WIDGET_CHANGE
+	                  , WIDGET_CLOSEALL
+                    };
 }
 
 //}
