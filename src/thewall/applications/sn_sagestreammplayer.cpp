3921d3e458d1607109d02d21be3c15bd90b2791d
Sungwon(loki@evl) <schiktol@gmail.com>
2012-11-07 14:47:55 -0600

    video player button position centered at the bottom

--- src/thewall/applications/sn_sagestreammplayer.cpp
+++ src/thewall/applications/sn_sagestreammplayer.cpp
@@ -10,29 +10,54 @@ SN_SageStreamMplayer::SN_SageStreamMplayer(const quint64 globalappid, const QSet
 
     QObject::connect(this, SIGNAL(streamerInitialized()), this, SLOT(setButtonPosition()));
 
-	_rewindButton = new SN_PixmapButton(":/resources/media-forward-rtl_128x128.png", 0, "", this);
-	_pauseButton = new SN_PixmapButton(":/resources/media-pause-128x128.png", 0, "", this);
-	_playButton = new SN_PixmapButton(":/resources/media-play-ltr_128x128.png", 0, "", this);
+	_rewindButton = new SN_PixmapButton(":/resources/media-forward-rtl_128x128.png", 64, "", this);
+	_pauseButton = new SN_PixmapButton(":/resources/media-pause-128x128.png", 64, "", this);
+	_playButton = new SN_PixmapButton(":/resources/media-play-ltr_128x128.png", 64, "", this);
 	_playButton->hide();
-	_fforwardButton = new SN_PixmapButton(":/resources/media-forward-ltr_128x128.png", 0, "", this);
+	_fforwardButton = new SN_PixmapButton(":/resources/media-forward-ltr_128x128.png", 64, "", this);
 
+	/*
 	_rewindButton->setPriorityOverride(1);
 	_playButton->setPriorityOverride(1);
 	_pauseButton->setPriorityOverride(-1);
 	_fforwardButton->setPriorityOverride(1);
+	*/
 
-	connect(_rewindButton, SIGNAL(clicked()), this, SLOT(rewindMplayer()));
-	connect(_pauseButton, SIGNAL(clicked()), this, SLOT(pauseMplayer()));
-	connect(_playButton, SIGNAL(clicked()), this, SLOT(playMplayer()));
-	connect(_fforwardButton, SIGNAL(clicked()), this, SLOT(fforwardMplayer()));
+	QObject::connect(_rewindButton, SIGNAL(clicked()), this, SLOT(rewindMplayer()));
+	QObject::connect(_pauseButton, SIGNAL(clicked()), this, SLOT(pauseMplayer()));
+	QObject::connect(_playButton, SIGNAL(clicked()), this, SLOT(playMplayer()));
+	QObject::connect(_fforwardButton, SIGNAL(clicked()), this, SLOT(fforwardMplayer()));
+
 }
 
 
 void SN_SageStreamMplayer::setButtonPosition() {
-    _rewindButton->setPos(0, boundingRect().bottom() - _rewindButton->size().height());
+    int buttonswidth = _rewindButton->size().width() + _playButton->size().width() + _fforwardButton->size().width();
+    _rewindButton->setPos((boundingRect().width() - buttonswidth)/2, boundingRect().bottom() - _rewindButton->size().height() - 4);
+
     _pauseButton->setPos(_rewindButton->geometry().topRight());
     _playButton->setPos(_rewindButton->geometry().topRight());
     _fforwardButton->setPos(_playButton->geometry().topRight());
+
+    /*
+    QGraphicsLayoutItem* blank = new QGraphicsLayoutItem;
+    blank->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    QGraphicsLinearLayout* btnlayout = new QGraphicsLinearLayout(Qt::Horizontal);
+	btnlayout->addItem(_rewindButton);
+	btnlayout->addItem(_pauseButton);
+	btnlayout->addItem(_playButton);
+	btnlayout->addItem(_fforwardButton);
+
+	QGraphicsLinearLayout* layout = new QGraphicsLinearLayout(Qt::Vertical);
+    layout->setMinimumSize(_appInfo->nativeSize());
+	layout->setAlignment(btnlayout, Qt::AlignHCenter | Qt::AlignBottom);
+
+    layout->addItem(blank);
+	layout->addItem(btnlayout);
+
+	setLayout(layout);
+    */
 }
 
 void SN_SageStreamMplayer::rewindMplayer() {
